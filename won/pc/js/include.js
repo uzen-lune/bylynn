10d46c568bc46b4ab516901b00327a2ea01a57ac
won <won@uzen.net>
2023-02-21 12:59:01 +0900

    include 추가

---
+++ won/pc/js/include.js
@@ -0,0 +1,14 @@
+$(function(){
+    var includes1 = $('[data-include1="header"]');
+    var includes2 = $('[data-include2="aside"]');
+    var includes3 = $('[data-include3="footer"]');
+    jQuery.each(includes1, function(){
+       $(this).load('header.html');
+    });
+    jQuery.each(includes2, function(){
+      $(this).load('aside.html');
+    });
+    jQuery.each(includes3, function(){
+      $(this).load('footer.html');
+    });
+  });
